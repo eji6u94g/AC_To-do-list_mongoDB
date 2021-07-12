f27d83bdf5a802f3ceb22ac3c409acb254235c53
eji6u94g <seanmybear@gmail.com>
2021-07-12 15:04:12 +0800

    feat: add "view pacific item detail info" feature

--- app.js
+++ app.js
@@ -34,13 +34,21 @@ app.get('/', (req, res) => {
 })
 
 app.get('/todos/new', (req, res) => {
-  return res.render('new')
+  res.render('new')
 })
 
 app.post('/todos', (req, res) => {
   const name = req.body.name
   todoData.create({ name })
     .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  todoData.findById(id)
+    .lean()
+    .then(todo => res.render('detail', { todo }))
     .catch(error => console.log(error))
 })
 
