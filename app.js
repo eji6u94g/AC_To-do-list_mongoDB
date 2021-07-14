5ba41e2a480847179611ae5170fbce2fe3dfc898
eji6u94g <seanmybear@gmail.com>
2021-07-14 17:03:46 +0800

    feat: add "sort each item in index page by time" feature

--- app.js
+++ app.js
@@ -25,10 +25,13 @@ app.set('view engine', 'handlebars')
 //set body-parser
 app.use(bodyParser.urlencoded({ extended: true }))
 
+
+//index page
 app.get('/', (req, res) => {
   //get all of data from Todo models
   todoData.find()
     .lean() //把 Mongoose 的 Model 物件轉換成乾淨單純的 JavaScript 資料陣列
+    .sort({ _id: 'asc' }) //依_id來排序資料, _id有生成前後順序差別, 所以也可以看成是根據創建時間排序
     .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
 })
